0fa3b9a13faf493ce75158d58b70f9bfc458f14c
ilya <ilaprokofev14@gmail.com>
2025-04-25 12:13:39 +0300

    Initial commit

---
+++ TestClass/Test1.cs
@@ -0,0 +1,61 @@
+﻿using Worktesting;
+
+namespace TestClass;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+[TestClass]
+public sealed class SortingTests
+{
+    // Тест для сортировки методом выбора
+    [TestMethod]
+    public void TestSelectionSort()
+    {
+        int[] sizes = { 1000, 5000, 500000 };
+
+        foreach (int size in sizes)
+        {
+            int[] array = GenerateRandomArray(size);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            // Вызов метода сортировки
+            ArraySorting.SelectionSort((int[])array.Clone());
+
+            stopwatch.Stop();
+            Console.WriteLine($"SelectionSort - Размер массива: {size}, Время: {stopwatch.ElapsedMilliseconds} мс");
+        }
+    }
+
+    // Тест для быстрой сортировки
+    [TestMethod]
+    public void TestQuickSort()
+    {
+        int[] sizes = { 1000, 5000, 500000 };
+
+        foreach (int size in sizes)
+        {
+            int[] array = GenerateRandomArray(size);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            // Вызов метода сортировки
+            ArraySorting.QuickSort((int[])array.Clone());
+
+            stopwatch.Stop();
+            Console.WriteLine($"QuickSort - Размер массива: {size}, Время: {stopwatch.ElapsedMilliseconds} мс");
+        }
+    }
+
+    // Метод для генерации случайного массива
+    private int[] GenerateRandomArray(int size)
+    {
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(1, 1000000); // Значения от 1 до 1,000,000
+        }
+        return array;
+    }
+}
---
+++ Worktesting/ArrSorting.cs
@@ -0,0 +1,78 @@
+﻿namespace Worktesting;
+
+using System;
+using System.Diagnostics;
+
+using System;
+
+public class ArraySorting
+{
+    // 1) Сортировка массива методом выбора с обменом
+    public static int[] SelectionSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            // Обмен элементов
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+        return array;
+    }
+
+    // 2) Быстрая сортировка массива
+    public static int[] QuickSort(int[] array)
+    {
+        QuickSortRecursive(array, 0, array.Length - 1);
+        return array;
+    }
+
+    // Вспомогательный рекурсивный метод для быстрой сортировки
+    private static void QuickSortRecursive(int[] array, int low, int high)
+    {
+        if (low < high)
+        {
+            int partitionIndex = Partition(array, low, high);
+
+            QuickSortRecursive(array, low, partitionIndex - 1);
+            QuickSortRecursive(array, partitionIndex + 1, high);
+        }
+    }
+
+    // Метод для разделения массива
+    private static int Partition(int[] array, int low, int high)
+    {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (array[j] <= pivot)
+            {
+                i++;
+                // Обмен элементов
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // Обмен опорного элемента
+        int tempPivot = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = tempPivot;
+
+        return i + 1;
+    }
+
+    
+}
